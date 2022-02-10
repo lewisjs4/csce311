9700eab6a7ec99f4532933ccc472555a4e9f6292
Lewis <lewisjs4@cse.sc.edu>
2022-02-09 21:52:54 -0500

    Basic code added for readers/writer problem

---
+++ readers_writers_1/inc/semaphore_manager.h
@@ -0,0 +1,44 @@
+// Copyright 2022 CSCE 311
+//
+#ifndef READERS_WRITERS_1_INC_SEMAPHORE_MANAGER_H_
+#define READERS_WRITERS_1_INC_SEMAPHORE_MANAGER_H_
+
+#include <pthread.h>  // POSIX threads (use flag -pthread in g++)
+#include <semaphore.h>  // POSIX semaphore support
+
+#include <cassert>  // assert
+#include <cstddef>  // size_t
+
+#include <vector>
+
+
+class ThreadSemaphoreManager {
+ public:
+  // When index equals number of stored semaphores, a new semaphore is
+  // created. Otherwise, a previous location gets reused, the original
+  // semaphore at that location destroyed.
+  static size_t Create(int count, size_t index = 0);
+
+
+  inline static void Destroy() {
+    for (::sem_t& mutex : semaphores_)
+      ::sem_destroy(&mutex);
+  }
+
+
+  inline static void Down(size_t index=0) {
+    ::sem_wait(&(semaphores_.at(index)));
+  }
+
+
+  inline static void Up(size_t index=0) {
+    ::sem_post(&(semaphores_.at(index)));
+  }
+
+
+ private:
+  static std::vector<::sem_t> semaphores_;
+
+};
+
+#endif  // READERS_WRITERS_1_INC_SEMAPHORE_MANAGER_H_
